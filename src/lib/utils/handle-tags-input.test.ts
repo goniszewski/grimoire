3a2ad640c0e46300b19942c0d020716d5b6c8cb6
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-08-25 13:02:06 +0200

    test: improve tests for 'handle-tags-input.ts'
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/lib/utils/handle-tags-input.test.ts
+++ src/lib/utils/handle-tags-input.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { prepareTags } from './handle-tags-input';
 
@@ -27,30 +27,77 @@ const createTagsStub = [
 	}
 ];
 
+const pbCreateArgsStub = {
+    first: [{
+        name: createTagsStub[0].label,
+        slug: createTagsStub[0].value,
+        owner: 'test'
+    },
+    {
+        $cancelKey: createTagsStub[0].label
+    }]
+    ,
+    second: [{
+        name: createTagsStub[1].label,
+        slug: createTagsStub[1].value,
+        owner: 'test'
+    },
+    {
+        $cancelKey: createTagsStub[1].label
+    }]
+}
+
+const pbCreateMock = vi.fn(() => ({
+    id: tagIdStub
+}));
+
+const pbCollectionMock = vi.fn(() =>  ({
+        create: pbCreateMock
+    })
+    );
+
 const pb = {
-	collection: () => ({
-		create: () => ({ id: tagIdStub })
-	})
+	collection: pbCollectionMock
 };
 
+
+
+
 describe('prepareTags', () => {
+    beforeEach(() => {
+        pbCreateMock.mockClear();
+        pbCollectionMock.mockClear();
+    });
+
 	it('should create tags', async () => {
 		const tags = await prepareTags(pb as any, createTagsStub, 'test');
+
 		expect(tags).toEqual([tagIdStub, tagIdStub]);
+        expect(pbCollectionMock).toHaveBeenCalledWith('tags');
+        expect(pbCreateMock).toHaveBeenCalledWith(...pbCreateArgsStub.first);
+        expect(pbCreateMock).toHaveBeenLastCalledWith(...pbCreateArgsStub.second);
+        expect(pbCollectionMock).toHaveBeenCalledTimes(2);
 	});
 
 	it('should return existing tags', async () => {
 		const tags = await prepareTags(pb as any, existingTagsStub, 'test');
+
 		expect(tags).toEqual([existingTagsStub[0].value, existingTagsStub[1].value]);
+        expect(pbCollectionMock).not.toHaveBeenCalled();
 	});
 
 	it('should return existing and created tags', async () => {
 		const tags = await prepareTags(pb as any, [...existingTagsStub, ...createTagsStub], 'test');
+        
 		expect(tags).toEqual([
 			existingTagsStub[0].value,
 			existingTagsStub[1].value,
 			tagIdStub,
 			tagIdStub
 		]);
+        expect(pbCollectionMock).toHaveBeenCalledWith('tags');
+        expect(pbCreateMock).toHaveBeenCalledWith(...pbCreateArgsStub.first);
+        expect(pbCreateMock).toHaveBeenLastCalledWith(...pbCreateArgsStub.second);
+        expect(pbCollectionMock).toHaveBeenCalledTimes(2);
 	});
 });
