41b8ea644a62ec33db08d52cbec931def969ae8a
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-10-31 11:46:02 +0100

    fix: handle rejection when creating new bookmark
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -33,25 +33,33 @@ export const actions = {
 		const tags = data.get('tags') ? JSON.parse(data.get('tags') as string) : [];
 
 		const tagIds = await prepareTags(pb, tags, owner);
-		const { id } = await pb.collection('bookmarks').create({
-			author,
-			category: category?.value ? category.value : category,
-			tags: tagIds,
-			content_html,
-			content_published_date,
-			content_text,
-			content_type,
-			description,
-			domain,
-			flagged,
-			icon_url,
-			importance,
-			main_image_url,
-			note,
-			owner,
-			title,
-			url
-		});
+		const { id } = await pb
+			.collection('bookmarks')
+			.create({
+				author,
+				category: category?.value ? category.value : category,
+				tags: tagIds,
+				content_html,
+				content_published_date,
+				content_text,
+				content_type,
+				description,
+				domain,
+				flagged,
+				icon_url,
+				importance,
+				main_image_url,
+				note,
+				owner,
+				title,
+				url
+			})
+			.catch((e) => {
+				console.error('Error creating bookmark', e);
+				return {
+					id: null
+				};
+			});
 
 		if (!id) {
 			return {
