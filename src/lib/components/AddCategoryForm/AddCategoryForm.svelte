549f44eb5324e5f62cbf8b04449c1d521831a2ef
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-11-17 13:39:57 +0100

    style: sync styling of 'AddCategoryForm' and 'EditCategoryForm'
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/lib/components/AddCategoryForm/AddCategoryForm.svelte
+++ src/lib/components/AddCategoryForm/AddCategoryForm.svelte
@@ -70,187 +70,174 @@
 	];
 </script>
 
-<form
-	bind:this={form}
-	method="POST"
-	action="/?/addNewCategory"
-	use:enhance={() =>
-		({ update, result }) => {
-			if (result.type === 'success') {
-				showToast.success('Category added', {
-					position: 'bottom-center'
-				});
-			}
+<div class="w-full">
+	<form
+		bind:this={form}
+		method="POST"
+		action="/?/addNewCategory"
+		use:enhance={() =>
+			({ update, result }) => {
+				if (result.type === 'success') {
+					showToast.success('Category added', {
+						position: 'bottom-center'
+					});
+				}
 
-			if (result.type === 'error') {
-				showToast.error(`Error: ${JSON.stringify(result?.error)}`, {
-					position: 'bottom-center'
-				});
-			}
-			closeModal();
-			update();
-		}}
->
-	<input type="text" class="hidden" name="id" value={$category.id} />
-	<input type="text" class="hidden" name="icon" value={$category.icon} />
-	<div class="flex flex-col w-full gap-2">
-		<div class="flex flex-col w-full">
-			<label for="name" class="label">Name</label>
-			<input
-				type="text"
-				class="input input-bordered"
-				name="name"
-				value={$category.name}
-				placeholder="School, Work, etc..."
-				required
-				on:input={(event) => {
-					// @ts-ignore-next-line
-					$category.name = event.target.value;
-				}}
-			/>
-		</div>
-		<div class="flex flex-col w-full">
-			<label for="icon" class="label">Icon</label>
-			<div class="flex gap-1">
-				{#each icons as icon}
-					<div
-						class={`btn btn-circle btn-ghost btn-sm flex justify-center items-center p-1
+				if (result.type === 'error') {
+					showToast.error(`Error: ${JSON.stringify(result?.error)}`, {
+						position: 'bottom-center'
+					});
+				}
+				closeModal();
+				update();
+			}}
+	>
+		<input type="text" class="hidden" name="id" value={$category.id} />
+		<input type="text" class="hidden" name="icon" value={$category.icon} />
+		<div class="flex flex-col w-full gap-2">
+			<div class="flex flex-col w-full">
+				<label for="name" class="label">Name</label>
+				<input
+					type="text"
+					class="input input-bordered"
+					name="name"
+					value={$category.name}
+					placeholder="School, Work, etc..."
+					required
+					on:input={(event) => {
+						// @ts-ignore-next-line
+						$category.name = event.target.value;
+					}}
+				/>
+			</div>
+			<div class="flex flex-col w-full">
+				<label for="icon" class="label">Icon</label>
+				<div class="flex gap-1">
+					{#each icons as icon}
+						<div
+							class={`btn btn-circle btn-ghost btn-sm flex justify-center items-center p-1
 								${$category.icon === icon.id ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''}
 								`}
-						title={icon.title}
-						role="button"
-						tabindex="0"
-						on:keydown={(event) => {
-							if (event.key === 'Enter') {
-								// @ts-ignore-next-line
-								$category.icon = icon.id;
-							}
-						}}
-						on:click={() => {
-							// @ts-ignore-next-line
-							$category.icon = icon.id;
-						}}
-					>
-						<Icon name={icon.id} />
-					</div>
-				{/each}
-				<div
-					class={`btn btn-ghost btn-sm flex justify-center items-center p-1 text-xs
-								${$category.icon === '' ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''}
-								`}
-					title="None"
-					role="button"
-					tabindex="0"
-					on:keydown={(event) => {
-						if (event.key === 'Enter') {
-							// @ts-ignore-next-line
-							$category.icon = '';
-						}
-					}}
-					on:click={() => {
-						// @ts-ignore-next-line
-						$category.icon = '';
-					}}
-				>
-					None
-				</div>
-			</div>
-		</div>
-		<div class="flex flex-col w-auto">
-			<label for="description" class="label">Description</label>
-			<textarea
-				class="textarea textarea-bordered"
-				name="description"
-				value={$category.description}
-				placeholder="What is this category about?"
-				on:change={(event) => {
-					// @ts-ignore-next-line
-					$category.description = event.target.value;
-				}}
-			/>
-		</div>
-		<div class="flex flex-col gap-2">
-			<label for="color" class="label">Color</label>
-
-			<div class="flex gap-2">
-				<div class="flex flex-wrap max-w-[12rem] gap-1">
-					{#each categoryColors as color}
-						<div
-							class={`w-6 h-6 rounded-full ${
-								$category.color === color ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''
-							}`}
-							style={`background-color: ${color};`}
+							title={icon.title}
 							role="button"
 							tabindex="0"
 							on:keydown={(event) => {
 								if (event.key === 'Enter') {
 									// @ts-ignore-next-line
-									$category.color = color;
+									$category.icon = icon.id;
 								}
 							}}
 							on:click={() => {
 								// @ts-ignore-next-line
-								$category.color = color;
+								$category.icon = icon.id;
+							}}
+						>
+							<Icon name={icon.id} />
+						</div>
+					{/each}
+					<div
+						class={`btn btn-ghost btn-sm flex justify-center items-center p-1 text-xs
+								${$category.icon === '' ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''}
+								`}
+						title="None"
+						role="button"
+						tabindex="0"
+						on:keydown={(event) => {
+							if (event.key === 'Enter') {
+								// @ts-ignore-next-line
+								$category.icon = '';
+							}
+						}}
+						on:click={() => {
+							// @ts-ignore-next-line
+							$category.icon = '';
+						}}
+					>
+						None
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-col w-auto">
+				<label for="description" class="label">Description</label>
+				<textarea
+					class="textarea textarea-bordered"
+					name="description"
+					value={$category.description}
+					placeholder="What is this category about?"
+					on:change={(event) => {
+						// @ts-ignore-next-line
+						$category.description = event.target.value;
+					}}
+				/>
+			</div>
+			<div class="flex flex-col gap-2">
+				<label for="color" class="label">Color</label>
+
+				<div class="flex gap-2">
+					<div class="flex flex-wrap max-w-[12rem] gap-1">
+						{#each categoryColors as color}
+							<div
+								class={`w-6 h-6 rounded-full ${
+									$category.color === color ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''
+								}`}
+								style={`background-color: ${color};`}
+								role="button"
+								tabindex="0"
+								on:keydown={(event) => {
+									if (event.key === 'Enter') {
+										// @ts-ignore-next-line
+										$category.color = color;
+									}
+								}}
+								on:click={() => {
+									// @ts-ignore-next-line
+									$category.color = color;
+								}}
+							/>
+						{/each}
+					</div>
+					<div class="join">
+						<div class="tooltip" data-tip="Color preview">
+							<div
+								class="join-item border border-slate-600 w-6 h-12 flex flex-col justify-end"
+								style={`background-color: ${$category.color};`}
+							/>
+						</div>
+						<input
+							type="text"
+							class="join-item input input-bordered w-9/12 backdrop-invert"
+							name="color"
+							value={$category.color}
+							placeholder="E.g. #00FFFF"
+							on:input={(event) => {
+								// @ts-ignore-next-line
+								$category.color = event.target.value;
 							}}
 						/>
-					{/each}
-				</div>
-
-				<div class="join">
-					<div class="tooltip" data-tip="Color preview">
-						<div
-							class="join-item border border-slate-600 w-6 h-12 flex flex-col justify-end"
-							style={`background-color: ${$category.color};`}
-						/>
 					</div>
-					<input
-						type="text"
-						class="join-item input input-bordered w-9/12 backdrop-invert"
-						name="color"
-						value={$category.color}
-						placeholder="E.g. #00FFFF"
-						on:input={(event) => {
+				</div>
+			</div>
+			<div class="flex flex-col w-auto">
+				<label for="parent" class="label">Parent</label>
+				<div class="tooltip" data-tip="Select parent category">
+					<Select
+						name="parent"
+						searchable
+						placeholder="Select parent category..."
+						required
+						value={$category.parent?.id || $categoriesOptions[0].value}
+						items={$categoriesOptions}
+						class="this-select input input-bordered w-max"
+						on:change={(event) => {
 							// @ts-ignore-next-line
-							$category.color = event.target.value;
+							$category.parent = $page.data.categories.find((c) => c.id === event.detail.value);
 						}}
 					/>
 				</div>
 			</div>
+			<button class="btn btn-primary my-6 mx-auto w-full max-w-xs" disabled={!$category.name}
+				>Save</button
+			>
 		</div>
-		<div class="flex flex-col w-auto">
-			<label for="parent" class="label">Parent</label>
-			<div class="tooltip" data-tip="Select parent category">
-				<Select
-					name="parent"
-					searchable
-					placeholder="Select parent category..."
-					required
-					value={$category.parent?.id || $categoriesOptions[0].value}
-					items={$categoriesOptions}
-					class="this-select input input-bordered w-max"
-					on:change={(event) => {
-						// @ts-ignore-next-line
-						$category.parent = $page.data.categories.find((c) => c.id === event.detail.value);
-					}}
-				/>
-			</div>
-		</div>
-		<!-- TODO: enable when public categories are implemented -->
-		<!-- <div class="flex flex-col w-full">
-				<label for="public" class="label">Public</label>
-				<input
-					type="checkbox"
-					class="checkbox checkbox-primary"
-					name="public"
-					checked={!!$category.public}
-					on:change={(event) => {
-						// @ts-ignore-next-line
-						$category.public = event.target.checked;
-					}}
-				/>
-			</div> -->
-		<button class="btn btn-primary my-6 mx-auto w-full max-w-xs" disabled={!$category.name}
-			>Save</button
-		>
-	</div>
-</form>
+	</form>
+</div>
--- src/lib/components/EditCategoryForm/EditCategoryForm.svelte
+++ src/lib/components/EditCategoryForm/EditCategoryForm.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import Select from 'svelte-select';
-	import { writable, type Writable } from 'svelte/store';
+	import { writable } from 'svelte/store';
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 
 	import { editCategoryStore } from '$lib/stores/edit-category.store';
-	import type { Category } from '$lib/interfaces/Category.interface';
 	import { icons } from '$lib/enums/icons';
 	import Icon from '$lib/components/Icon/Icon.svelte';
 	import { showToast } from '$lib/utils/show-toast';
+	import type { Category } from '$lib/types/Category.type';
 
 	const category = writable<Partial<Category>>({});
 	$: $category = { ...$editCategoryStore };
@@ -40,7 +40,22 @@
 		];
 	}
 
-	let error = '';
+	const categoryColors = [
+		'#FBEAA3',
+		'#76C5EF',
+		'#23E4FC',
+		'#80CA90',
+		'#FDD411',
+		'#E7866E',
+		'#57B55F',
+		'#D97FBE',
+		'#FE7197',
+		'#C278FF',
+		'#497AF3',
+		'#E91074',
+		'#844561',
+		'#20164E'
+	];
 </script>
 
 {#if $category?.id}
@@ -69,7 +84,7 @@
 		>
 			<input type="text" class="hidden" name="id" value={$category.id} />
 			<input type="text" class="hidden" name="icon" value={$category.icon} />
-			<div class="flex flex-col w-full">
+			<div class="flex flex-col w-full gap-2">
 				<div class="flex flex-col w-full">
 					<label for="name" class="label">Name</label>
 					<input
@@ -77,6 +92,7 @@
 						class="input input-bordered"
 						name="name"
 						value={$category.name}
+						placeholder="School, Work, etc..."
 						required
 						on:input={(event) => {
 							// @ts-ignore-next-line
@@ -89,7 +105,7 @@
 					<div class="flex gap-1">
 						{#each icons as icon}
 							<div
-								class={`badge badge-outline cursor-pointer rounded-full w-8 h-8 flex justify-center items-center p-1
+								class={`btn btn-circle btn-ghost btn-sm flex justify-center items-center p-1
 								${$category.icon === icon.id ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''}
 								`}
 								title={icon.title}
@@ -109,6 +125,26 @@
 								<Icon name={icon.id} />
 							</div>
 						{/each}
+						<div
+							class={`btn btn-ghost btn-sm flex justify-center items-center p-1 text-xs
+									${$category.icon === '' ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''}
+									`}
+							title="None"
+							role="button"
+							tabindex="0"
+							on:keydown={(event) => {
+								if (event.key === 'Enter') {
+									// @ts-ignore-next-line
+									$category.icon = '';
+								}
+							}}
+							on:click={() => {
+								// @ts-ignore-next-line
+								$category.icon = '';
+							}}
+						>
+							None
+						</div>
 					</div>
 				</div>
 				<div class="flex flex-col w-auto">
@@ -117,6 +153,7 @@
 						class="textarea textarea-bordered"
 						name="description"
 						value={$category.description}
+						placeholder="What is this category about?"
 						on:change={(event) => {
 							// @ts-ignore-next-line
 							$category.description = event.target.value;
@@ -127,57 +164,66 @@
 					<label for="color" class="label">Color</label>
 
 					<div class="flex gap-2">
-						{#each ['#ff0000', '#ff8000', '#ffff00', '#80ff00', '#00ff00', '#00ff80', '#00ffff', '#0080ff', '#0000ff', '#8000ff', '#ff00ff', '#ff0080', '#ff0000'] as color}
-							<div
-								class="w-6 h-6 rounded-full"
-								style={`background-color: ${color};`}
-								on:click={() => {
+						<div class="flex flex-wrap max-w-[12rem] gap-1">
+							{#each categoryColors as color}
+								<div
+									class={`w-6 h-6 rounded-full ${
+										$category.color === color ? 'ring-primary ring-2 bg-primary bg-opacity-20' : ''
+									}`}
+									style={`background-color: ${color};`}
+									role="button"
+									tabindex="0"
+									on:keydown={(event) => {
+										if (event.key === 'Enter') {
+											// @ts-ignore-next-line
+											$category.color = color;
+										}
+									}}
+									on:click={() => {
+										// @ts-ignore-next-line
+										$category.color = color;
+									}}
+								/>
+							{/each}
+						</div>
+						<div class="join">
+							<div class="tooltip" data-tip="Color preview">
+								<div
+									class="join-item border border-slate-600 w-6 h-12 flex flex-col justify-end"
+									style={`background-color: ${$category.color};`}
+								/>
+							</div>
+							<input
+								type="text"
+								class="join-item input input-bordered w-9/12 backdrop-invert"
+								name="color"
+								value={$category.color}
+								placeholder="E.g. #00FFFF"
+								on:input={(event) => {
 									// @ts-ignore-next-line
-									$category.color = color;
+									$category.color = event.target.value;
 								}}
 							/>
-						{/each}
-
-						<input
-							type="text"
-							class="input input-bordered w-9/12"
-							name="color"
-							value={$category.color}
-							on:input={(event) => {
-								// @ts-ignore-next-line
-								$category.color = event.target.value;
-							}}
-						/>
+						</div>
 					</div>
 				</div>
 				<div class="flex flex-col w-auto">
 					<label for="parent" class="label">Parent</label>
-					<Select
-						name="parent"
-						searchable
-						placeholder="Select parent category..."
-						required
-						value={$category.parent?.id || $categoriesOptions[0].value}
-						items={$categoriesOptions}
-						class="this-select input input-bordered w-max"
-						on:change={(event) => {
-							// @ts-ignore-next-line
-							$category.parent = $page.data.categories.find((c) => c.id === event.detail.value);
-						}}
-					/>
-				</div>
-				<div class="flex flex-col w-full">
-					<label for="public" class="label">Public</label>
-					<input
-						type="checkbox"
-						class="checkbox checkbox-primary"
-						name="public"
-						checked={!!$category.public}
-						on:change={(event) => {
-							// @ts-ignore-next-line
-							$category.public = event.target.checked;
-						}}
-					/>
+					<div class="tooltip" data-tip="Select parent category">
+						<Select
+							name="parent"
+							searchable
+							placeholder="Select parent category..."
+							required
+							value={$category.parent?.id || $categoriesOptions[0].value}
+							items={$categoriesOptions}
+							class="this-select input input-bordered w-max"
+							on:change={(event) => {
+								// @ts-ignore-next-line
+								$category.parent = $page.data.categories.find((c) => c.id === event.detail.value);
+							}}
+						/>
+					</div>
 				</div>
 			</div>
 			<div>
