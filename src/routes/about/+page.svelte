0428423c329843cd28ef7ad57d52fce0f3b04949
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-11-17 19:27:19 +0100

    feat: overhaul About page
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,33 +1,68 @@
 <div class="w-full">
 	<h1 class="text-2xl font-bold">About</h1>
-	<p class="mt-4">Bookmarking service for developers.</p>
-	<div class="flex flex-col justify-center items-center">
-		<h2 class="text-xl font-bold mt-8 mb-4">Developers and contributors</h2>
-		<div class="flex flex-col gap-2">
-			<div class="flex max-w-[14rem] items-center">
-				<a href="https://www.github.com/goniszewski" class="link text-center">
-					Robert Goniszewski (@goniszewski)
-				</a>
-				<div class="badge badge-outline">author</div>
+	<div class="flex w-full flex-wrap mb-14">
+		<div class="flex flex-none flex-col indent-8 gap-2 mt-4 max-w-fit md:max-w-xl">
+			<div class="flex justify-center items-center mt-8 mb-6">
+				<img
+					srcset="grimoire_logo_300.webp 300w,
+					grimoire_logo_600.webp 600w,
+					grimoire_logo.webp 960w"
+					alt="Grimoire logo"
+					class="max-w-[20rem]"
+				/>
+			</div>
+			<p>
+				Glimpse into the magical book of <em>your</em> forbidden knowledge -
+			</p>
+			<p class="mx-auto my-2">
+				<strong class="text-3xl font-serif"><em>Grimoire</em></strong>! 📖💫
+			</p>
+			<p>
+				Unleash your inner sorcerer and conquer the chaos of bookmarks! With Grimoire, you'll have a
+				bewitching way to store and sort your enchanted links.
+			</p>
+			<strong>But wait, there's more!</strong>
+			<p>
+				Transmute your saved pages into juicy content snippets with our mystical extraction feature.
+				Embrace the magic, tame the clutter, and let Grimoire be your mystical companion in the vast
+				library of the web.
+			</p>
+			<p>It's time to conjure up some organization! 📚✨</p>
+		</div>
+		<div class="flex flex-col flex-1 min-w-fit">
+			<div class="flex flex-col justify-center items-center">
+				<h2 class="text-xl font-bold mt-8 mb-4">Developers and contributors</h2>
+				<div class="flex flex-col gap-2">
+					<div class="flex max-w-[14rem] items-center">
+						<div class="indicator">
+							<span class="indicator-item indicator-top badge badge-primary">author</span>
+							<a href="https://www.github.com/goniszewski" class="link text-center p-1">
+								Robert Goniszewski (@goniszewski)
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-col items-center mt-8">
+				<h2 class="text-xl font-bold mt-8 mb-4">Special thanks to</h2>
+				<p class="flex flex-col items-center gap-2">
+					<a href="https://github.com/saadeghi/daisyui" class="link">DaisyUI Team</a>
+					<a href="https://github.com/krisk/fuse" class="link">Fuse.js Team</a>
+					<a href="https://github.com/microlinkhq/metascraper" class="link">MetaScraper Team</a>
+					<a href="https://github.com/pocketbase/pocketbase" class="link">PocketBase Team</a>
+					<a href="https://github.com/apostrophecms/sanitize-html" class="link"
+						>sanitize-html Team</a
+					>
+					<a href="https://github.com/sveltejs/kit" class="link">SvelteKit Team</a>
+					<a href="https://github.com/rob-balfre/svelte-select" class="link">Svelte Select Team</a>
+					<a href="https://github.com/kbrgl/svelte-french-toast" class="link"
+						>Svelte French Toast Team</a
+					>
+					<a href="https://github.com/extractus/article-extractor" class="link">
+						@extractus/article-extractor Team</a
+					>
+				</p>
 			</div>
 		</div>
-	</div>
-	<div class="flex flex-col items-center mt-8">
-		<h2 class="text-xl font-bold mt-8 mb-4">Special thanks to</h2>
-		<p class="flex flex-col items-center gap-2">
-			<a href="https://github.com/saadeghi/daisyui" class="link">DaisyUI Team</a>
-			<a href="https://github.com/krisk/fuse" class="link">Fuse.js Team</a>
-			<a href="https://github.com/microlinkhq/metascraper" class="link">MetaScraper Team</a>
-			<a href="https://github.com/pocketbase/pocketbase" class="link">PocketBase Team</a>
-			<a href="https://github.com/apostrophecms/sanitize-html" class="link">sanitize-html Team</a>
-			<a href="https://github.com/sveltejs/kit" class="link">SvelteKit Team</a>
-			<a href="https://github.com/rob-balfre/svelte-select" class="link">Svelte Select Team</a>
-			<a href="https://github.com/kbrgl/svelte-french-toast" class="link"
-				>Svelte French Toast Team</a
-			>
-			<a href="https://github.com/extractus/article-extractor" class="link">
-				@extractus/article-extractor Team</a
-			>
-		</p>
 	</div>
 </div>
