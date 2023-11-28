9aa5301d22151663070e4798ac0eab39b39f8cbe
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-11-28 16:18:11 +0100

    chore: create WIP helpers to handle indexes for search
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/lib/utils/search.ts
+++ src/lib/utils/search.ts
@@ -1,5 +1,9 @@
+import { pb } from '$lib/pb';
+import fs from 'fs';
 import Fuse from 'fuse.js';
+import path from 'path';
 
+import type { FuseIndex, IFuseOptions } from 'fuse.js';
 const defaultOptions = {
 	includeScore: true,
 	shouldSort: true,
@@ -27,9 +31,54 @@ const defaultOptions = {
 		}
 	]
 };
+const indexKeys = defaultOptions.keys.map((key) => key.name);
 
-export const searchFactory = (data: any[], options: Fuse.IFuseOptions<any> = {}) => {
-	const fuse = new Fuse(data, { ...defaultOptions, ...options });
+export const searchFactory = (
+	data: any[],
+	options: IFuseOptions<any> = {},
+	index: FuseIndex<any>
+) => {
+	const fuse = new Fuse(data, { ...defaultOptions, ...options }, index);
 
 	return fuse;
+};
+
+export const createSearchIndex = (data: any[], userId: string) => {
+	const createdIndex = Fuse.createIndex(indexKeys, data);
+
+	fs.writeFileSync(
+		path.resolve(process.cwd(), 'indexes', `search-index-${userId}.json`),
+		JSON.stringify(createdIndex)
+	);
+
+	return createdIndex;
+};
+
+export const loadSearchIndex = (userId: string): FuseIndex<any> => {
+	const index = fs.readFileSync(
+		path.resolve(process.cwd(), 'indexes', `search-index-${userId}.json`),
+		'utf-8'
+	);
+
+	return JSON.parse(index);
+};
+
+export const initializeSearch = async (userId: string) => {
+	let index: FuseIndex<any>;
+	let bookmarksForIndex = [] as Record<string, any>[];
+
+	index = loadSearchIndex(userId);
+
+	if (index === null) {
+		bookmarksForIndex = await pb.collection('bookmarks').getFullList({
+			fields: 'id,title,domain,description,url,tags.name',
+			expand: 'tags.name',
+			filter: `owner.id="${userId}"`,
+			batchSize: 100000
+		});
+
+		index = createSearchIndex(bookmarksForIndex, userId);
+	}
+
+	return searchFactory(bookmarksForIndex, {}, index);
 };
