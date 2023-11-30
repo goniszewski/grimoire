ec704b043760a3577404ecd6f264d5f88bbd03d4
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-11-30 19:11:59 +0100

    fix: resolve critical issues with config for dev/prod environments
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,16 @@
+import { env } from '$env/dynamic/public';
+
+const getProcessEnvValue = (key: string) =>
+	typeof process === 'object' ? process.env[key] : undefined;
+
 export default {
-	BACKEND_URL: process.env.PUBLIC_POCKETBASE_URL || 'http://localhost:8090',
-	POCKETBASE_URL: process.env.PUBLIC_POCKETBASE_URL || 'http://pocketbase:80',
-	HTTPS_ONLY: process.env.HTTPS_ONLY === 'true' || false
+	BACKEND_URL:
+		env.PUBLIC_POCKETBASE_URL ||
+		getProcessEnvValue('PUBLIC_POCKETBASE_URL') ||
+		'http://localhost:8090',
+	POCKETBASE_URL:
+		env.PUBLIC_POCKETBASE_URL ||
+		getProcessEnvValue('PUBLIC_POCKETBASE_URL') ||
+		'http://pocketbase:80',
+	HTTPS_ONLY: env.PUBLIC_HTTPS_ONLY === 'true' || getProcessEnvValue('PUBLIC_HTTPS_ONLY') || false
 };
