15fab9660eebb59f393bebe6e40fa91dd8825839
Robert Goniszewski <robert.goniszewski@noaignite.com>
2023-12-13 16:05:49 +0100

    feat: add initial API methods for '/api/bookmarks'
    Signed-off-by: Robert Goniszewski <robert.goniszewski@noaignite.com>

--- src/routes/api/bookmarks/+server.ts
+++ src/routes/api/bookmarks/+server.ts
@@ -1,7 +1,9 @@
 import { pb } from '$lib/pb.js';
-import { getFileUrl } from '$lib/utils';
+import { getFileUrl, prepareTags } from '$lib/utils';
 
 import { json } from '@sveltejs/kit';
+
+import type { Bookmark } from '$lib/types/Bookmark.type.js';
 
 export async function GET({ locals, url }) {
 	const owner = locals.user?.id;
@@ -39,6 +41,227 @@ export async function GET({ locals, url }) {
 
 		return json(
 			{ bookmarks },
+			{
+				status: 200
+			}
+		);
+	} catch (error: any) {
+		return json(
+			{
+				success: false,
+				error: error?.message
+			},
+			{
+				status: 500
+			}
+		);
+	}
+}
+
+export async function POST({ locals, request }) {
+	const owner = locals.user?.id;
+
+	if (!owner) {
+		return json(
+			{
+				success: false,
+				error: 'Unauthorized'
+			},
+			{
+				status: 401
+			}
+		);
+	}
+
+	const requestBody = await request.json();
+
+	// TODO: validate request body
+
+	try {
+		const tags = requestBody.tags || [];
+		const tagIds = await prepareTags(pb, tags, owner);
+
+		const bookmark = (await pb.collection('bookmarks').create({
+			owner,
+			content_html: requestBody.content_html,
+			content_published_date: requestBody.content_published_date,
+			content_text: requestBody.content_text,
+			content_type: requestBody.content_type,
+			description: requestBody.description,
+			domain: requestBody.domain,
+			icon_url: requestBody.icon_url,
+			importance: requestBody.importance,
+			main_image_url: requestBody.main_image_url,
+			note: requestBody.note,
+			title: requestBody.title,
+			url: requestBody.url,
+			author: requestBody.author,
+			tags: tagIds,
+			flagged: requestBody.flagged ? new Date().toISOString() : null
+		})) as Bookmark;
+
+		if (!bookmark.id) {
+			return json(
+				{
+					success: false,
+					error: 'Bookmark creation failed'
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		if (requestBody.main_image_url || requestBody.icon_url) {
+			const attachments = new FormData();
+
+			if (requestBody.main_image_url) {
+				const main_image = await fetch(requestBody.main_image_url as string).then((r) => r.blob());
+				attachments.append('main_image', main_image);
+			}
+
+			if (requestBody.icon_url) {
+				const icon = await fetch(requestBody.icon_url as string).then((r) => r.blob());
+				attachments.append('icon', icon);
+			}
+
+			await pb.collection('bookmarks').update(bookmark.id, attachments);
+		}
+
+		return json(
+			{ bookmark },
+			{
+				status: 200
+			}
+		);
+	} catch (error: any) {
+		return json(
+			{
+				success: false,
+				error: error?.message
+			},
+			{
+				status: 500
+			}
+		);
+	}
+}
+
+export async function PATCH({ locals, request }) {
+	const owner = locals.user?.id;
+
+	if (!owner) {
+		return json(
+			{
+				success: false,
+				error: 'Unauthorized'
+			},
+			{
+				status: 401
+			}
+		);
+	}
+
+	const requestBody = await request.json();
+
+	const { id, ...updatedFields } = requestBody;
+
+	// TODO: validate updated fields
+
+	try {
+		const currentBookmark = await pb.collection('bookmarks').getOne(id);
+
+		if (!currentBookmark) {
+			return json(
+				{
+					success: false,
+					error: 'Bookmark not found'
+				},
+				{
+					status: 404
+				}
+			);
+		}
+
+		const tags = requestBody.tags || [];
+
+		const tagIds = await prepareTags(pb, tags, owner);
+
+		const bookmark = (await pb.collection('bookmarks').update(id, {
+			...updatedFields,
+			tags: tagIds,
+			flagged: requestBody.flagged ? new Date().toISOString() : null
+		})) as Bookmark;
+
+		if (!bookmark.id) {
+			return json(
+				{
+					success: false,
+					error: 'Bookmark update failed'
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		if (requestBody.main_image_url || requestBody.icon_url) {
+			const attachments = new FormData();
+
+			if (requestBody.main_image_url) {
+				const main_image = await fetch(requestBody.main_image_url as string).then((r) => r.blob());
+				attachments.append('main_image', main_image);
+			}
+
+			if (requestBody.icon_url) {
+				const icon = await fetch(requestBody.icon_url as string).then((r) => r.blob());
+				attachments.append('icon', icon);
+			}
+
+			await pb.collection('bookmarks').update(bookmark.id, attachments);
+		}
+
+		return json(
+			{ bookmark },
+			{
+				status: 200
+			}
+		);
+	} catch (error: any) {
+		return json(
+			{
+				success: false,
+				error: error?.message
+			},
+			{
+				status: 500
+			}
+		);
+	}
+}
+
+export async function DELETE({ locals, request }) {
+	const owner = locals.user?.id;
+
+	if (!owner) {
+		return json(
+			{
+				success: false,
+				error: 'Unauthorized'
+			},
+			{
+				status: 401
+			}
+		);
+	}
+
+	const { id } = await request.json();
+
+	try {
+		const result = await pb.collection('bookmarks').delete(id);
+
+		return json(
+			{ result },
 			{
 				status: 200
 			}
