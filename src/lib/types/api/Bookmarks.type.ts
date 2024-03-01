65a53712194a267297f0695d5811f5d46284c6d6
Robert Goniszewski <robertgoniszewski@outlook.com>
2024-03-01 19:37:44 +0100

    chore: add DB field with config for handling page screenshots
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

--- src/lib/types/api/Bookmarks.type.ts
+++ src/lib/types/api/Bookmarks.type.ts
@@ -14,6 +14,7 @@ export type AddBookmarkRequestBody = {
 	flagged?: boolean;
 	category: string;
 	tags?: string[];
+	screenshot?: string;
 };
 
 export type UpdateBookmarkRequestBody = AddBookmarkRequestBody & {
