c9679b40e29376d0a6428857c613ce430bfd3ee9
Robert Goniszewski <robertgoniszewski@outlook.com>
2024-08-21 21:03:29 +0200

    style(about): improve layout and add new technology credits
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,8 @@
 <div class="w-full">
 	<h1 class="text-2xl font-bold">About</h1>
-	<div class="flex w-full flex-wrap mb-14">
-		<div class="flex flex-none flex-col indent-8 gap-2 mt-4 max-w-fit md:max-w-xl">
-			<div class="flex justify-center items-center mt-8 mb-6">
+	<div class="mb-14 flex w-full flex-wrap">
+		<div class="mt-4 flex max-w-fit flex-none flex-col gap-2 indent-8 md:max-w-xl">
+			<div class="mb-6 mt-8 flex items-center justify-center">
 				<img
 					srcset="grimoire_logo_300.webp 300w,
 					grimoire_logo_600.webp 600w,
@@ -15,7 +15,7 @@
 				Glimpse into the magical book of <em>your</em> forbidden knowledge -
 			</p>
 			<p class="mx-auto my-2">
-				<strong class="text-3xl font-serif"><em>Grimoire!</em></strong> 📖💫
+				<strong class="font-serif text-3xl"><em>Grimoire!</em></strong> 📖💫
 			</p>
 			<p>
 				Unleash your inner sorcerer and conquer the chaos of bookmarks! With Grimoire, you'll have a
@@ -31,27 +31,29 @@
 			</p>
 			<p>It's time to conjure up some organization! 📚✨</p>
 		</div>
-		<div class="flex flex-col flex-1 min-w-fit">
-			<div class="flex flex-col justify-center items-center">
-				<h2 class="text-xl font-bold mt-8 mb-4">Developers and contributors</h2>
+		<div class="flex min-w-fit flex-1 flex-col">
+			<div class="flex flex-col items-center justify-center">
+				<h2 class="mb-4 mt-8 text-xl font-bold">Developers and contributors</h2>
 				<div class="flex flex-col gap-2">
 					<div class="flex max-w-[14rem] items-center">
 						<div class="indicator">
-							<span class="indicator-item indicator-top badge badge-primary badge-sm">author</span>
-							<a href="https://www.github.com/goniszewski" class="link text-center p-1">
+							<span class="badge indicator-item badge-primary badge-sm indicator-top">author</span>
+							<a href="https://www.github.com/goniszewski" class="link p-1 text-center">
 								Robert Goniszewski (@goniszewski)
 							</a>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="flex flex-col items-center mt-8">
-				<h2 class="text-xl font-bold mt-8 mb-4">Special thanks to</h2>
+			<div class="mt-8 flex flex-col items-center">
+				<h2 class="mb-4 mt-8 text-xl font-bold">Special thanks to</h2>
 				<p class="flex flex-col items-center gap-2">
 					<a href="https://github.com/extractus/article-extractor" class="link">
 						@extractus/article-extractor</a
 					>
+					<a href="https://github.com/oven-sh/bun" class="link">Bun</a>
 					<a href="https://github.com/saadeghi/daisyui" class="link">DaisyUI</a>
+					<a href="https://github.com/drizzle-team/drizzle-orm" class="link">Drizzle</a>
 					<a href="https://github.com/krisk/fuse" class="link">Fuse.js</a>
 					<a href="https://github.com/microlinkhq/metascraper" class="link">MetaScraper</a>
 					<a href="https://github.com/pocketbase/pocketbase" class="link">PocketBase</a>
@@ -61,6 +63,7 @@
 					>
 					<a href="https://github.com/rob-balfre/svelte-select" class="link">Svelte Select</a>
 					<a href="https://tailwindcss.com" class="link">Tailwind CSS</a>
+					<a href="https://github.com/laurengarcia/url-metadata" class="link">url-metadata</a>
 				</p>
 			</div>
 		</div>
