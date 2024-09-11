4a7f78ba2f4c26908514a906b3a603a04348dd50
Robert Goniszewski <43510122+goniszewski@users.noreply.github.com>
2024-09-11 20:31:12 +0200

    refactor(metadata): improve urlMetadataScraper function and error han… (#120)
    * refactor(metadata): improve urlMetadataScraper function and error handling; fix error on missing
    
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>
    
    * feat(metadata): improve icon URL validation and error handling
    
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>
    
    * fix(auth): improve login error handling and messages
    
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>
    
    ---------
    
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

--- src/lib/utils/get-metadata.ts
+++ src/lib/utils/get-metadata.ts
@@ -6,19 +6,27 @@ import { extract, extractFromHtml } from '@extractus/article-extractor';
 
 import type { Metadata } from '$lib/types/Metadata.type';
 
-const getIconUrl = (pageUrl: string, iconPath: string): string => {
-	const baseUrl = new URL(pageUrl).origin;
-	iconPath = iconPath.split('?')[0];
+const getIconUrl = async (pageUrl: string, iconPath: string): Promise<string> => {
+	if (!iconPath) return '';
 
-	if (iconPath.startsWith('/')) {
-		iconPath = `${baseUrl}${iconPath.replace('//', '/')}`;
-	} else if (iconPath.startsWith('./')) {
-		iconPath = `${baseUrl}${iconPath.replace('./', '/')}`;
-	} else if (!iconPath.startsWith('http')) {
-		iconPath = `${baseUrl}/${iconPath}`;
+	const baseUrl = new URL(pageUrl).origin.replace(/\/$/, '');
+	iconPath = iconPath.split('?')[0].replace(/^\//, '');
+
+	let fullIconUrl = iconPath.startsWith('http') ? iconPath : `${baseUrl}/${iconPath}`;
+
+	try {
+		const response = await fetch(fullIconUrl, { method: 'HEAD' });
+		if (response.ok) {
+			const contentType = response.headers.get('content-type');
+			if (contentType && contentType.startsWith('image/')) {
+				return fullIconUrl;
+			}
+		}
+	} catch (error) {
+		console.error('Error checking icon URL:', error);
 	}
 
-	return iconPath || '';
+	return '';
 };
 
 const removeExtraEmptyLines = (html: string) => html.replace(/\n\s*\n/g, '\n');
@@ -56,45 +64,44 @@ const articleExtractorScraper = async (html: string, url: string): Promise<Parti
 };
 
 async function urlMetadataScraper(html: string, url: string): Promise<Partial<Metadata>> {
-	let urlMetadataMetadata: urlMetadata.Result;
+	try {
+		const urlMetadataMetadata =
+			html !== ''
+				? await urlMetadata(null, {
+						parseResponseObject: new Response(html, { headers: { 'content-type': 'text/html' } })
+					})
+				: await urlMetadata(url);
 
-	if (html !== '') {
-		const response = new Response(html, {
-			headers: {
-				'content-type': 'text/html'
-			}
-		});
-		urlMetadataMetadata = await urlMetadata(null, {
-			parseResponseObject: response
-		});
-	} else {
-		urlMetadataMetadata = await urlMetadata(url);
+		const iconUrls = urlMetadataMetadata?.favicons
+			?.map((favicon: { href: string; sizes?: string }) => {
+				const isPng = favicon.href.endsWith('.png');
+				const faviconSize = +(favicon.sizes?.split('x')?.[0] ?? '0');
+				const isBestSize = faviconSize && (faviconSize <= 120 || faviconSize >= 64);
+				return isPng && isBestSize ? favicon.href : null;
+			})
+			.filter(Boolean);
+
+		const iconUrl = await getIconUrl(
+			url,
+			iconUrls?.[0] || urlMetadataMetadata?.favicons?.[0]?.href || ''
+		);
+
+		return {
+			url: urlMetadataMetadata?.url || urlMetadataMetadata?.['og:url'],
+			title: urlMetadataMetadata?.title || urlMetadataMetadata?.['og:title'],
+			description: urlMetadataMetadata?.description || urlMetadataMetadata?.['og:description'],
+			author: urlMetadataMetadata?.author || urlMetadataMetadata?.['twitter:creator'],
+			mainImageUrl: urlMetadataMetadata?.image || urlMetadataMetadata?.['og:image'],
+			iconUrl
+		};
+	} catch (error) {
+		console.error('Error in urlMetadataScraper:', error);
+		return { url };
 	}
-
-	const iconUrls = urlMetadataMetadata?.favicons
-		?.map((favicon: { rel: string; href: string; sizes: string }) => {
-			const isPng = favicon.href.endsWith('.png');
-			const faviconSize = +favicon.sizes?.split('x')?.[0] || 0;
-			const isBestSize = faviconSize && (faviconSize <= 120 || faviconSize >= 64);
-
-			if (isPng && isBestSize) {
-				return favicon.href;
-			}
-		})
-		.filter(Boolean);
-
-	return {
-		url: urlMetadataMetadata?.url || urlMetadataMetadata?.['og:url'],
-		title: urlMetadataMetadata?.title || urlMetadataMetadata?.['og:title'],
-		description: urlMetadataMetadata?.description || urlMetadataMetadata?.['og:description'],
-		author: urlMetadataMetadata?.author || urlMetadataMetadata?.['twitter:creator'],
-		mainImageUrl: urlMetadataMetadata?.image || urlMetadataMetadata?.['og:image'],
-		iconUrl: getIconUrl(url, iconUrls[0] || urlMetadataMetadata?.favicons?.[0].href)
-	};
 }
 
 export async function getMetadata(url: string, providedHtml?: string): Promise<Metadata> {
-	const html: string = providedHtml || (await fetch(url).then((res) => res.text()));
+	const html: string = providedHtml || (await fetch(url).then((res: Response) => res.text()));
 
 	const urlMetadataMetadata = await urlMetadataScraper(html, url);
 	const articleExtractorMetadata = await articleExtractorScraper(html, url);
@@ -106,7 +113,7 @@ export async function getMetadata(url: string, providedHtml?: string): Promise<M
 		: '';
 
 	return {
-		url: urlMetadataMetadata?.url || articleExtractorMetadata?.url || '',
+		url: urlMetadataMetadata?.url || articleExtractorMetadata?.url || url,
 		domain,
 		title: urlMetadataMetadata?.title || articleExtractorMetadata?.title || '',
 		description:
