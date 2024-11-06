c842b99f7d3beaa83faf532b000b222fcabfafa2
Robert Goniszewski <robertgoniszewski@outlook.com>
2024-11-06 16:39:45 +0100

    refactor(api): migrate Swagger UI to external documentation and enhance health endpoint
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

--- src/routes/api/health/+server.ts
+++ src/routes/api/health/+server.ts
@@ -1,11 +1,18 @@
+import { version } from '$app/environment';
+import { getUserCount } from '$lib/database/repositories/User.repository';
+
 import { json } from '@sveltejs/kit';
 
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
+	const dbConnected = await getUserCount().then((count) => count >= 0);
+
 	return json(
 		{
-			currentServerTime: new Date().toISOString()
+			currentServerTime: new Date().toISOString(),
+			database: dbConnected ? 'CONNECTED' : 'FAILED TO CONNECT',
+			appVersion: version
 		},
 		{
 			status: 200
